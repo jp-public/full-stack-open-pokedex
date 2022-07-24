0ba7caf0621478111427257601a53d89a9188fbc
juhopaakkanen <juho.paakkanen@gmail.com>
2022-07-24 14:53:29 +0300

    Fix lint errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,8 @@ module.exports = {
   env: {
     browser: true,
     es6: true,
-    'jest/globals': true
+    'jest/globals': true,
+    node: true
   },
   extends: ['eslint:recommended', 'plugin:react/recommended'],
   parserOptions: {
@@ -22,7 +23,7 @@ module.exports = {
     'no-trailing-spaces': 'error',
     'object-curly-spacing': ['error', 'always'],
     'arrow-spacing': ['error', { before: true, after: true }],
-    'no-console': 'false',
+    'no-console': 0,
     'react/prop-types': 0
   }
 }
